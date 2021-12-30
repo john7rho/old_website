e3c7d5380562582257e6a8ad81013125872f6a46
John Rho <johnrho21@gmail.com>
2021-12-29 22:33:34 -0600

    Updates

---
+++ precache-manifest.5270694d58d755a70a6c13dbdb41a997.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e71a7674dcee16e12c25c9d77e93a88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74fdd47a878c95cad94b",
+    "url": "/static/css/2.7ff34c0e.chunk.css"
+  },
+  {
+    "revision": "08be518ad2a2ca71057a",
+    "url": "/static/css/main.0de4aa73.chunk.css"
+  },
+  {
+    "revision": "74fdd47a878c95cad94b",
+    "url": "/static/js/2.a5cee2af.chunk.js"
+  },
+  {
+    "revision": "08be518ad2a2ca71057a",
+    "url": "/static/js/main.eaa57d82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dbbf02b60985825da0472baf6c8a1443",
+    "url": "/static/media/ListenIn.dbbf02b6.png"
+  },
+  {
+    "revision": "45d585d83b0387d1ea61db3e7c3b863f",
+    "url": "/static/media/OrpheusLogo_square.45d585d8.png"
+  },
+  {
+    "revision": "00f03f3d99d804c42743ff3bbb922c89",
+    "url": "/static/media/Rho-John-5.00f03f3d.jpg"
+  },
+  {
+    "revision": "e7dfc8512865d95d3ac651d057136284",
+    "url": "/static/media/loading_house.e7dfc851.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8ca33e2d6d6da667bcb1ebcab471014.js"
+  "/precache-manifest.5270694d58d755a70a6c13dbdb41a997.js"
 );
 
 self.addEventListener('message', (event) => {
